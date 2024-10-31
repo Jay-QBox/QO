5ee1df973d51614f3755bb3f5019f4b5745122d5
Jaymin Rabari <jayminrabari1108@gmail.com>
2024-11-01 01:51:04 +0530

    imgs + data added in cards

--- frontend/src/assets/imgs/img.jsx
+++ frontend/src/assets/imgs/img.jsx
@@ -1,4 +1,4 @@
-// src/assets/images/images.js - created by jaymin - add comment for new changes
+// src/assets/images/images.jsx - created by jaymin - add comment for new changes
 import call from './callcenter.webp';
 import customer from './customer.webp';
 import Hospitals_AV from './Hospitals_AV.webp';
@@ -12,6 +12,47 @@ import Food_Services_3_AV from './Food_Services_3_AV.webp';
 import AuthorisedAttenders_AV from './AuthorisedAttenders_AV.webp';
 import Medical_Packages_AV from './Medical_Packages_AV.webp';
 import Surgical_Packages_AV from './Surgical Packages_AV.webp';
+// AG_HOMECARE_POST_PRE_HOSPITAL_SERVICES - by jaymin
+import AG_Palliative_care from './AG_1/AG_Palliative_care.webp';
+import AG_Homecare_Post_Pre_Hospital_Services from './AG_1/AG_Homecare_Post_Pre_Hospital_Services.webp';
+import AG_Care_Taker_Patient_Attender from './AG_1/AG_Care_Taker_Patient_Attender.webp';
+import AG_Post_operative_Care from './AG_1/AG_Post_operative_Care.webp';
+import AG_ICU_home_care_setup from './AG_1/AG_ICU_home_care_setup.webp';
+import AG_General_ICU_Nursing_care from './AG_1/AG_General_ICU_Nursing_care.webp';
+
+// AG_2 - by jaymin
+import AG_Doctor_on_Tele from './AG_2/AG_Doctor_on_Tele.webp';
+import AG_Homecare_Med_services from './AG_2/AG_Homecare_Med_services.webp';
+import AG_Psychiatric_Tele from './AG_2/AG_Psychiatric_Tele.webp';
+import AG_Physiotherapy_Tele from './AG_2/AG_Physiotherapy_Tele.webp';
+
+// AG_hospital_equipment - by jaymin
+import AG_Air from './AG_hospital_equipment/AG_Air.webp';
+import AG_Ambulatory_BP from './AG_hospital_equipment/AG_Ambulatory_BP.webp';
+import AG_Chemotherapy from './AG_hospital_equipment/AG_Chemotherapy.webp';
+import AG_Home_Xray from './AG_hospital_equipment/AG_Home_Xray.webp';
+import AG_Homecare_Hospital from './AG_hospital_equipment/AG_Homecare_Hospital.webp';
+import AG_Medical_Devices from './AG_hospital_equipment/AG_Medical_Devices.webp';
+import AG_Mobility_Aid from './AG_hospital_equipment/AG_Mobility_Aid.webp';
+import AG_Oxygen_Concentrator from './AG_hospital_equipment/AG_Oxygen_Concentrator.webp';
+import AG_Ryles_Tube from './AG_hospital_equipment/AG_Ryles_Tube.webp';
+import AG_Urine_Catheter_Setup from './AG_hospital_equipment/AG_Urine_Catheter_Setup.webp';
+import AG_Vaccinations_Home from './AG_hospital_equipment/AG_Vaccinations_Home.webp';
+import AG_Hospital_Beds from './AG_hospital_equipment/AG_Hospital_Beds.webp';
+import AG_IV_Infusion_Setup from './AG_hospital_equipment/AG_IV_Infusion_Setup.webp';
+
+// AG_medical_emergency - by jaymin
+import AG_Ambulance_support from './AG_medical_emergency/AG_Ambulance_support.webp';
+import AG_Emergency_coordination from './AG_medical_emergency/AG_Emergency_coordination.webp';
+import AG_Homecare_Medical from './AG_medical_emergency/AG_Homecare_Medical.webp';
+
+// AG_medical_monitoring - by jaymin
+import AG_Homecare_Medical_Monitoring_devices from './AG_medical_monitoring/AG_Homecare_Medical _Monitoring_devices.webp';
+import Homecare_Medical_BP from './AG_medical_monitoring/Homecare_Medical_BP.webp';
+import Homecare_Medical_Diabetics from './AG_medical_monitoring/Homecare_Medical_Diabetics.webp';
+import Homecare_Medical_Vital from './AG_medical_monitoring/Homecare_Medical_Vital.webp';
+import Homecare_Medical_Heart from './AG_medical_monitoring/Homecare_Medical_Heart.webp';
+
 
 const images = {
   call,
@@ -27,6 +68,42 @@ const images = {
   AuthorisedAttenders_AV,
   Medical_Packages_AV,
   Surgical_Packages_AV,
+  // AG_HOMECARE_POST_PRE_HOSPITAL_SERVICES - by jaymin
+  AG_Palliative_care,
+  AG_Homecare_Post_Pre_Hospital_Services,
+  AG_Care_Taker_Patient_Attender,
+  AG_Post_operative_Care,
+  AG_ICU_home_care_setup,
+  AG_General_ICU_Nursing_care,
+
+  AG_Doctor_on_Tele,
+  AG_Homecare_Med_services,
+  AG_Psychiatric_Tele,
+  AG_Physiotherapy_Tele,
+
+  AG_Air,
+  AG_Ambulatory_BP,
+  AG_Chemotherapy,
+  AG_Home_Xray,
+  AG_Homecare_Hospital,
+  AG_Medical_Devices,
+  AG_Mobility_Aid,
+  AG_Oxygen_Concentrator,
+  AG_Ryles_Tube,
+  AG_Urine_Catheter_Setup,
+  AG_Vaccinations_Home,
+  AG_Hospital_Beds,
+  AG_IV_Infusion_Setup,
+
+  AG_Ambulance_support,
+  AG_Emergency_coordination,
+  AG_Homecare_Medical,
+
+  AG_Homecare_Medical_Monitoring_devices,
+  Homecare_Medical_BP,
+  Homecare_Medical_Diabetics,
+  Homecare_Medical_Vital,
+  Homecare_Medical_Heart,
 };
 
 export default images;
